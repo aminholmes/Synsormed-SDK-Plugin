ff1ab0e2e25cc749e2386fa561389969b8db4105
Amin <aholmes@synsormed.com>
2019-01-22 20:46:46 -0500

    add connection code

--- ios/sdkIOSPlugin.m
+++ ios/sdkIOSPlugin.m
@@ -6,7 +6,11 @@
 
 - (void) init:(CDVInvokedUrlCommand *) command
 {
-	NSLog(@"I am going to initQB");
+	NSLog(@"I am going to init in sdkIOSPlugin");
+
+	[CRCreativeSDK sharedInstance].delegate = self;
+    [CRSpo2 sharedInstance].delegate = self;
+
 	[[self commandDelegate]sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"]
 		callbackId:command.callbackId];
 }
@@ -14,6 +18,18 @@
 - (void) CMI_POD1W_Connect:(CDVInvokedUrlCommand *) command
 {
 	CMI_POD1W_Connect_Callback = command.callbackId;
+
+	NSString* UUIDString = [command.arguments objectAtIndex:0];
+	NSLog(@"The incoming UUID is: %@" , UUIDString);
+	NSUUID *myUUID = [[NSUUID alloc] initWithUUIDString:UUIDString];
+	NSLog(@"*** I created a NSUUID");
+	CreativePeripheral *myPeripheral;
+	NSLog(@"*** I created a blank myPeripheral");
+	myPeripheral.myUUID = myUUID;
+	NSLog(@"*** I assing the UUID");
+
+
+	[[CRCreativeSDK sharedInstance] connectDevice:myPeripheral];
 }
 
 - (void) CMI_POD1W_Disconnect:(CDVInvokedUrlCommand *) command
@@ -40,7 +56,27 @@
 
 #pragma mark - Delegate Methods
 
+-(void)crManager:(CRCreativeSDK *)crManager OnConnected:(CreativePeripheral *)peripheral withResult:(resultCodeType)result CurrentCharacteristic:(CBCharacteristic *)theCurrentCharacteristic{
+    if (result == RESULT_SUCCESS) {
+        NSLog(@"*** connection success");
+        /*
+        [[CRSpo2 sharedInstance] QueryDeviceVer:currentPort];
+        
+        [[CRSpo2 sharedInstance] SetParamAction:TRUE port:currentPort];
+        
+        [[CRSpo2 sharedInstance] SetWaveAction:TRUE port:currentPort];
+        */
+        
+        
+    }
+    
+}
 
+-(void)crManager:(CRCreativeSDK *)crManager OnConnectFail:(CBPeripheral *)port{
+    NSLog(@"*** Connection failed");
+
+        
+}
 
 
 @end
