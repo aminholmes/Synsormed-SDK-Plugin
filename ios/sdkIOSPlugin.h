a5ec29df5e8222a738a8cbfaeb30424ada1514b7
Amin <aholmes@synsormed.com>
2019-01-22 19:14:26 -0500

    Set up base IOS support

--- ios/sdkIOSPlugin.h
+++ ios/sdkIOSPlugin.h
@@ -1,7 +1,21 @@
-#import <Cordova/CDV.h>
+#import <Cordova/CDVPlugin.h>
+#import <CreativePeripheral.h>
+#import <CRCreativeSDK.h>
+#import <CRCommon.h>
+#import <CRSpo2.h>
 
 
 @interface sdkIOSPlugin : CDVPlugin
+{
+
+	NSString *CMI_POD1W_Connect_Callback;
+	NSString *CMI_POD1W_Subscribe_Callback;
+	NSString *CMI_POD1W_Disconnect_Callback;
+	NSString CMI_POD1W_Unsubscribe_Callback;
+
+	BOOL CMI_POD1W_Connected;
+	
+}
 
 
 @end
--- ios/sdkIOSPlugin.m
+++ ios/sdkIOSPlugin.m
@@ -1,6 +1,44 @@
 #import "sdkIOSPlugin.h"
 
-@implementation sdkIOSPlugin
+@implementation sdkIOSPlugin{
+	
+}
+
+- (void) init:(CDInvokedUrlCommand *) command
+{
+	NSLog(@"I am going to initQB");
+	[[self commandDelegate]sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"]
+		callbackId:command.callbackId];
+}
+
+- (void) CMI_POD1W_Connect:(CDInvokedUrlCommand *) command
+{
+	CMI_POD1W_Connect_Callback = command.callbackId;
+}
+
+- (void) CMI_POD1W_Disconnect:(CDInvokedUrlCommand *) command
+{
+
+	CMI_POD1W_Disconnect_Callback = command.callbackId;
+
+}
+
+- (void) CMI_POD1W_Subscribe:(CDInvokedUrlCommand *) command
+{
+
+	CMI_POD1W_Subscribe_Callback = command.callbackId;
+
+}
+
+- (void) CMI_POD1W_Unsubscribe:(CDInvokedUrlCommand *) command
+{
+
+	CMI_POD1W_Unsubscribe_Callback = command.callbackId;
+
+}
+
+
+#pragma mark - Delegate Methods
 
 
 
